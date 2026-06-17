3c2574ab306d533d319dcec5988b5ed7118a952f
Ambika-20 <ambikasoundharan@gmail.com>
2026-06-17 22:25:36 +0530

    Created simple h1 tag and p tag in App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <h1>📝 My Todo App</h1>
+
+      <p>
+        My first React project built while learning React, Git & GitHub 🚀
+      </p>
+    </div>
+  );
+}
+
+export default App;
