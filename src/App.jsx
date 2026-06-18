5d836f95e49da00113409437a96e20444ce09071
Ambika-20 <ambikasoundharan@gmail.com>
2026-06-18 22:32:45 +0530

    Added add task functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,18 @@
 function App() {
+
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  function addTask() {
+    if (task.trim() === "") {
+      return;
+    }
+
+    setTasks([...tasks, task]);
+
+    setTask("");
+  }
+
   return (
     <div>
       <h1>📝 My Todo App</h1>
@@ -9,16 +23,21 @@ function App() {
         <input
           type="text"
           placeholder="Enter a task..."
+          value={task}
+          onChange={(e) => setTask(e.target.value)} 
         />
 
-        <button>Add</button>
+        <button onClick={addTask}>Add</button>
       </div>
 
       <h2>📋 Tasks</h2>
 
       <ul>
-        <li>Learn React</li>
-        <li>Build Todo App</li>
+        {tasks.map((item, index) => (
+          <li key={index}>
+            {item}
+          </li>
+        ))}
       </ul>
     </div>
   );
